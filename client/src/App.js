0c696becf8512cfbf429dcf35ce26c968eb6d157
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-05-05 16:57:00 +0200

    Indent using 4 spaces

--- client/src/App.js
+++ client/src/App.js
@@ -11,80 +11,80 @@ import './App.css';
 const { api, getStatus } = initCACCL();
 
 class App extends Component {
-  /**
-   * Initialize App component
-   */
-  constructor(props) {
-    super(props);
+    /**
+     * Initialize App component
+     */
+    constructor(props) {
+        super(props);
 
-    // Set up state
-    this.state = {
-      message: `Loading! Just a moment...`,
-    };
-  }
-
-  /**
-   * Called when the component mounted, pulls state and user profile from server
-   */
-  async componentDidMount() {
-    // Load status
-    try {
-      // Get status from server
-      const status = await getStatus();
-
-      // > App wasn't launched via Canvas
-      if (!status.launched) {
-        return this.setState({
-          message: `Please launch this app from Canvas.`,
-        });
-      }
-
-      // > App is not authorized
-      if (!status.authorized) {
-        return this.setState({
-          message: `We don't have access to Canvas. Please re-launch the app.`,
-        });
-      }
-    } catch (err) {
-      return this.setState({
-        message: `Error while requesting state from server: ${err.message}`,
-      });
+        // Set up state
+        this.state = {
+            message: `Loading! Just a moment...`,
+        };
     }
 
-    // Load profile information
-    try {
-      // Get profile from Canvas via api
-      const profile = await api.user.self.getProfile();
+    /**
+     * Called when the component mounted, pulls state and user profile from server
+     */
+    async componentDidMount() {
+        // Load status
+        try {
+            // Get status from server
+            const status = await getStatus();
 
-      // Update state
-      return this.setState({
-        message: `Hi ${profile.name}! Your CACCL app is ready!`,
-      });
-    } catch (err) {
-      return this.setState({
-        message: `Error while requesting user profile: ${err.message}`,
-      });
+            // > App wasn't launched via Canvas
+            if (!status.launched) {
+                return this.setState({
+                    message: `Please launch this app from Canvas.`,
+                });
+            }
+
+            // > App is not authorized
+            if (!status.authorized) {
+                return this.setState({
+                    message: `We don't have access to Canvas. Please re-launch the app.`,
+                });
+            }
+        } catch (err) {
+            return this.setState({
+                message: `Error while requesting state from server: ${err.message}`,
+            });
+        }
+
+        // Load profile information
+        try {
+            // Get profile from Canvas via api
+            const profile = await api.user.self.getProfile();
+
+            // Update state
+            return this.setState({
+                message: `Hi ${profile.name}! Your CACCL app is ready!`,
+            });
+        } catch (err) {
+            return this.setState({
+                message: `Error while requesting user profile: ${err.message}`,
+            });
+        }
     }
-  }
 
-  /**
-   * Render the App
-   */
-  render() {
-    // Deconstruct the state
-    const { message } = this.state;
+    /**
+     * Render the App
+     */
+    render() {
+        // Deconstruct the state
+        const { message } = this.state;
 
-    // Render the component
-    return (
-      <div className="app">
-        <header className="app-header">
-          <p>
-            <strong>{message}</strong>
-          </p>
-        </header>
-      </div>
-    );
-  }
+        // Render the component
+        return (
+            <div className="app">
+                <header className="app-header">
+                    <p>
+                        <strong>{message}</strong>
+                    </p>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
