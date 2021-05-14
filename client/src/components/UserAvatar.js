b7547d8344e77c11fb8744f405fe8da510784b04
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-05-14 20:36:06 +0200

    fix: Use empty string as default name

--- client/src/components/UserAvatar.js
+++ client/src/components/UserAvatar.js
@@ -5,8 +5,8 @@ import UserProfileContext from '../contexts/UserProfileContext';
 
 export default function UserAvatar({ size }) {
     const profile = useContext(UserProfileContext);
-    const name = profile?.name || 'Guest';
     const avatarUrl = profile?.avatar_url;
+    const name = profile?.name || '';
 
     return (
         <Avatar name={name} src={avatarUrl} size={size} />
