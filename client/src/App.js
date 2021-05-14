b48be571f0007c75baffff450872bae6c589a089
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-05-14 19:42:46 +0200

    Move theme provider to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import { View } from '@instructure/ui';
 
 import { LtiProvider } from './contexts/LtiContext';
+import { EmotionThemeProvider } from '@instructure/emotion'
 import { UserProfileProvider } from './contexts/UserProfileContext';
 
 import Alerts from './components/Alerts';
 import TopNav from './components/TopNav';
 import Content from './components/Content'
+
+import { canvas } from '@instructure/ui-themes'
 
 import styles from './App.module.css';
 
@@ -14,13 +17,15 @@ export default function App() {
     return (
         <LtiProvider>
             <UserProfileProvider>
-                <View as="main" padding="small">
-                    <div id={styles.alerts}>
-                        <Alerts />
-                    </div>
-                    <TopNav />
-                    <Content />
-                </View>
+                <EmotionThemeProvider theme={canvas}>
+                    <View as="main" padding="small">
+                        <div id={styles.alerts}>
+                            <Alerts />
+                        </div>
+                        <TopNav />
+                        <Content />
+                    </View>
+                </EmotionThemeProvider>
             </UserProfileProvider>
         </LtiProvider>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-import { canvas } from '@instructure/ui-themes'
-import { EmotionThemeProvider } from '@instructure/emotion'
+import './index.css';
 
 
 ReactDOM.render(
-    <EmotionThemeProvider theme={canvas}>
-        <App />
-    </EmotionThemeProvider>,
+    <App />,
     document.getElementById('root')
 );
