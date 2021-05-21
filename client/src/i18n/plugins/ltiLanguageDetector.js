87aa6fafe1b1beed94312193884c60d671121c17
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-05-21 21:44:07 +0200

    Fallback to browser language

--- client/src/i18n/plugins/ltiLanguageDetector.js
+++ client/src/i18n/plugins/ltiLanguageDetector.js
@@ -6,12 +6,16 @@ export const ltiLanguageDetector = {
     async: true,
     init: (services, detectorOptions, i18nextOptions) => { },
     detect: (callback) => {
+        const browserLang = window.navigator.language;
+
         getStatus().then(status => {
-            const lang = status?.launchInfo?.locale;
+            // Get language from LTI launch info, fallback to browser language
+            const lang = status?.launchInfo?.locale || browserLang;
 
             callback(lang);
         }).catch(err => {
-            callback();
+            // Fallback to browser language on error
+            callback(browserLang);
         });
     },
     cacheUserLanguage: (lang) => { }
