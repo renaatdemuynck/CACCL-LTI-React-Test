d58d02b56ed3cccef201678283946b9e40a3a246
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2021-05-23 21:25:21 +0200

    Translate LTI error messages

--- client/src/contexts/LtiContext.js
+++ client/src/contexts/LtiContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 import { getStatus } from '../caccl';
 
@@ -9,23 +10,24 @@ const LtiContext = createContext();
 export const LtiProvider = ({ children }) => {
     const [status, setStatus] = useState();
     const [err, setErr] = useState();
+    const { t } = useTranslation();
 
     useEffect(() => {
         getStatus().then(status => {
             // App wasn't launched via Canvas
             if (!status.launched) {
-                setErr(new Error(`Please launch this app from Canvas.`));
+                setErr(new Error(t('lti.error.launch_from_canvas')));
             }
             // App is not authorized
             else if (!status.authorized) {
-                setErr(new Error(`We don't have access to Canvas. Please re-launch the app.`));
+                setErr(new Error(t('lti.error.no_canvas_access')));
             }
 
-            setStatus(status)
+            setStatus(status);
         }).catch(err => { // App couldn't communicate with Canvas
-            setErr(new Error(`Error while requesting state from server: ${err.message}`));
+            setErr(new Error(t('lti.error.server') + `: ${err.message}`));
         });
-    }, []);
+    }, [t]);
 
     return (
         <LtiContext.Provider value={{ status, err }}>
